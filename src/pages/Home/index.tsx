51e23347a4947abb3bce03ce96f3e37b1aef8789
overkam <superivan0755@gmail.com>
2022-02-15 16:06:11 +0300

    add font Gilroy + Banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,13 @@ import { VFC } from 'react';
 import { Header } from 'components';
 
 import styles from './styles.module.scss';
+import { Banner } from './components';
 
 const Home: VFC = () => {
   return (
     <div className={styles.home}>
       <Header />
+      <Banner />
     </div>
   );
 };
