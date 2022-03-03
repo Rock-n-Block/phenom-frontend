ae35192352e13926f7bb8e8ae532d7a96dacda33
overkam <superivan0755@gmail.com>
2022-03-03 15:58:43 +0300

    11 types of different actions (layout only)

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as usePopover } from './usePopover';
 export { default as useWindowSize } from './useWindowSize';
 export { default as useClasses } from './useClasses';
+export { default as useGetUserAccessForNft } from './useGetUserAccessForNft';
---
+++ src/hooks/useGetUserAccessForNft.ts
@@ -0,0 +1,115 @@
+import React from 'react';
+import { INft, IOwner, TNullable } from 'types';
+
+export default (nft: TNullable<INft>, userId: string | number) => {
+  const isOwner = React.useMemo(() => {
+    if (userId && nft && nft.owners) {
+      if (Array.isArray(nft.owners)) {
+        return !!nft.owners.find((owner: IOwner) => {
+          return owner.id === userId;
+        });
+      }
+      return userId === nft.owners.id;
+    }
+    return false;
+  }, [nft, userId]);
+
+  const isUserCanRemoveFromSale = React.useMemo(() => {
+    if (userId && nft) {
+      if (
+        nft.standart === 'ERC721' &&
+        (nft.is_selling || nft.is_auc_selling || nft.is_timed_auc_selling) &&
+        isOwner
+      ) {
+        return true;
+      }
+      if (
+        nft.standart === 'ERC1155' &&
+        nft.sellers.find((seller) => seller.id === userId) &&
+        isOwner
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }, [nft, isOwner, userId]);
+
+  const isUserCanChangePrice = React.useMemo(() => {
+    if (userId && nft) {
+      if (nft.is_selling && isOwner) {
+        return true;
+      }
+    }
+    return false;
+  }, [nft, isOwner, userId]);
+
+  const isUserCanBuyNft = React.useMemo(() => {
+    if (userId && nft && nft.price && nft.is_selling && nft.available !== 0) {
+      if (nft.standart === 'ERC721' && !isOwner) {
+        return true;
+      }
+      if (
+        nft.standart === 'ERC1155' &&
+        ((nft.sellers.length === 1 && nft.sellers[0].id !== userId) || nft.sellers.length > 1)
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }, [nft, userId, isOwner]);
+
+  const isUserCanEnterInAuction = React.useMemo(() => {
+    if (userId && nft && (nft.is_auc_selling || nft?.is_timed_auc_selling) && nft.available !== 0) {
+      if (nft.standart === 'ERC721' && !isOwner) {
+        return true;
+      }
+    }
+    return false;
+  }, [nft, isOwner, userId]);
+
+  const isUserCanEndAuction = React.useMemo(() => {
+    if (
+      userId &&
+      nft &&
+      nft.is_auc_selling &&
+      !nft.is_timed_auc_selling &&
+      nft.bids.length &&
+      isOwner
+    ) {
+      if (nft.standart === 'ERC721') {
+        return true;
+      }
+      if (nft.standart === 'ERC1155' && nft.highest_bid?.bidder_id !== userId) {
+        return true;
+      }
+    }
+    return false;
+  }, [nft, isOwner, userId]);
+
+  const isUserCanPutOnSale = React.useMemo(() => {
+    if (userId && nft && isOwner) {
+      if (
+        nft.standart === 'ERC721' &&
+        !nft.is_selling &&
+        !nft.is_auc_selling &&
+        !nft.start_auction
+      ) {
+        return true;
+      }
+      if (nft.standart === 'ERC1155' && !nft.sellers.find((seller) => seller.id === userId)) {
+        return true;
+      }
+    }
+    return false;
+  }, [nft, isOwner, userId]);
+
+  return {
+    isOwner,
+    isUserCanRemoveFromSale,
+    isUserCanBuyNft,
+    isUserCanEnterInAuction,
+    isUserCanPutOnSale,
+    isUserCanEndAuction,
+    isUserCanChangePrice,
+  };
+};
