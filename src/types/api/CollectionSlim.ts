c1e7e4187a5989fbb0dd97926fa3c047cf03db3f
overkam <superivan0755@gmail.com>
2022-03-09 16:11:11 +0300

    resolve conflict

--- src/types/api/CollectionSlim.ts
+++ src/types/api/CollectionSlim.ts
@@ -5,19 +5,6 @@
  * This file was automatically generated.
  */
 
-<<<<<<< HEAD
-import { DisplayTheme } from './enums';
-
-export interface CollectionSlim {
-  address?: string;
-  avatar?: string;
-  displayTheme?: DisplayTheme;
-  id?: string;
-  isDefault?: boolean;
-  isNsfw?: boolean;
-  name?: string;
-}
-=======
 import { Display_theme } from "./enums";
 
 
@@ -30,4 +17,3 @@ export interface CollectionSlim {
     isNsfw?: boolean;
     name?: string;
 }
->>>>>>> b977ea64aed328490150ce323b3e1c12661608f5
